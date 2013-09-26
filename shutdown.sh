1240c74b190674fd177986ad7019e0e1a5d4a203
Benoit Pierre <benoit.pierre@gmail.com>
2013-09-26 05:21:32 +0200

    shutdown: add support for systemd.
    Detect and use systemd if present, instead of ConsoleKit.

--- shutdown.sh
+++ shutdown.sh
@@ -1,29 +1,75 @@
 #! /bin/sh
 
+dry_run=0
+mode=''
+
+run()
+{
+  if [ 0 -eq $dry_run ]
+  then
+    "$@"
+  else
+    echo "$@"
+  fi
+}
+
+# ConsoleKit support. {{{
+
 cs_action()
 {
-  dbus-send \
+  run dbus-send \
   --system --dest=org.freedesktop.ConsoleKit \
   --type=method_call --print-reply --reply-timeout=2000 \
   /org/freedesktop/ConsoleKit/Manager \
   "org.freedesktop.ConsoleKit.Manager.$1"
 }
 
-reboot()
+cs_reboot()
 {
-  echo 'rebooting'
   cs_action Restart
 }
 
-halt()
+cs_halt()
 {
-  echo 'shutting down'
   cs_action Stop
 }
 
+# }}}
+
+
+# systemd support. {{{
+
+sd_reboot()
+{
+  run systemctl reboot
+}
+
+sd_halt()
+{
+  run systemctl poweroff
+}
+
+# }}}
+
+if which systemctl >/dev/null 2>&1
+then
+  mode='sd'
+else
+  mode='cs'
+fi
+
 case "$1" in
-  -r) reboot ;;
-  -h|'') halt ;;
-  *) exit 2 ;;
+  -h|'')
+  echo 'shutting down'
+    "$mode"_halt
+    ;;
+  -r)
+  echo 'rebooting'
+    "$mode"_reboot
+    ;;
+  *)
+    exit 2
+    ;;
 esac
 
+# vim: foldmethod=marker
