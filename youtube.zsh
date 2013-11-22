ef85d468fbf9a036feabaa827d4b51fc96ed67ce
Benoit Pierre <benoit.pierre@gmail.com>
2013-11-22 16:43:13 +0100

    youtube: simplify quality handling for MPV.
    No need to use quvi, just pass the parameter to MPV.

--- youtube.zsh
+++ youtube.zsh
@@ -56,61 +56,8 @@ player_youtube_mpv()
 {
   quality="$1"
   video="$2"
-  url="http://www.youtube.com/watch?v=$video"
-  typeset -A formats
 
-  for format in `quvi --verbosity=quiet --query-formats "$url" | sed -n '/^\(\(fmt[0-9]\+_[^-| ]\+|\?\)\+\) : .*$/{s//\1/;s/|/ /gp}'`
-  do
-    case "$format" in
-      fmt102_720p)
-        # WebM 720p VP8 3D.
-        continue
-        ;;
-      fmt84_720p)
-        # MP4 720p H.264 3D.
-        continue
-        ;;
-      *_480p)
-        ;;
-      *_720p)
-        ;;
-      *_1080p)
-        ;;
-      *)
-        continue
-        ;;
-    esac
-    formats[$format]="$format"
-  done
-
-  [[ 1 -eq $verbose ]] && echo "formats: ${formats[@]}"
-
-  format=''
-  while [[ -z "$format" ]]
-  do
-    case "$quality" in
-      480p)
-        # fmt35_480p: 480p FLV
-        # fmt44_480p: 480p WebM
-        format="${formats[fmt44_480p]:-${formats[fmt35_480p]}}"
-        [[ -z "$format" ]] && format='default' # Use default format...
-        ;;
-      720p)
-        # fmt22_720p: 720p MP4
-        # fmt45_720p: 720p WebM
-        format="${formats[fmt45_720p]:-${formats[fmt22_720p]}}"
-        [[ -z "$format" ]] && quality='480p' # Try a lower quality.
-        ;;
-      1080p)
-        # fmt37_1080p: 1080p MP4
-        # fmt46_1080p: 1080p WebM
-        format="${formats[fmt46_1080p]:-${formats[fmt37_1080p]}}"
-        [[ -z "$format" ]] && quality='720p' # Try a lower quality.
-        ;;
-    esac
-  done
-
-  cmd_run mp-play --player=mpv --profile='youtube' --option=--quvi-format="$format" "http://www.youtube.com/watch?v=$video"
+  cmd_run mp-play --player=mpv --profile='youtube' --option=--quvi-format="$quality" "http://www.youtube.com/watch?v=$video"
 }
 
 while [ -n "$1" ]
